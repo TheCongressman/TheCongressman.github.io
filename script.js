f7755fedc9ed91a103fc3028e0aa8c3b8ad98f3d
TheCongressman <TheCongressman1@gmail.com>
2025-02-09 21:26:09 -0500

    css update

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ Array.from(document.querySelectorAll(".artwork-thumbnail")).forEach(item => {
       modalImage.src = event.target.src;
    });
 });
-document.querySelector(".close, .modal").addEventListener("click", () => {
+document.querySelector(".close").addEventListener("click", () => {
    modalEle.style.display = "none";
-   modalImage.src = '';
 });
+document.querySelector(".modal").addEventListener("click", () => {
+   modalEle.style.display = "none";
+});
+
+// 
+// var modalEle = document.querySelector(".modal");
+// var modalImage = document.querySelector(".modalImage");
+// var galleryItems = document.getElementsByClassName(".artwork-thumbnail");
+// Array.from(elements).forEach(function(galleryItem) {
+//   galleryItem.addEventListener("click", () => {
+//       modalEle.style.display = "flex";
+//       modalImage.src = event.target.src;
+//   });
+// });
+//
+// document.querySelector(".close, .modal").addEventListener("click", () => {
+//     modalEle.style.display = "none";
+//     modalImage.src = '';
+// });
