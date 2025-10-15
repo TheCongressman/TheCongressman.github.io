3558be67de340237c5ddce328c886138e3572bc7
TheCongressman <TheCongressman1@gmail.com>
2025-10-15 00:19:08 -0400

    modal rework

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 var modalEle = document.querySelector(".modal");
 var modalImage = document.querySelector(".modalImage");
+var body = document.querySelector("body");
 Array.from(document.querySelectorAll(".artwork-thumbnail")).forEach(item => {
     item.addEventListener("click", event => {
-      modalEle.style.display = "flex";
+      modalEle.style.display = "block";
       modalImage.src = event.target.src;
+      body.style.overflow = "hidden";
    });
 });
 document.querySelector(".close").addEventListener("click", () => {
    modalEle.style.display = "none";
+   body.style.overflow = "unset";
 });
 document.querySelector(".modal").addEventListener("click", () => {
    modalEle.style.display = "none";
+   body.style.overflow = "unset";
 });
 
-// 
+//
 // var modalEle = document.querySelector(".modal");
 // var modalImage = document.querySelector(".modalImage");
 // var galleryItems = document.getElementsByClassName(".artwork-thumbnail");
